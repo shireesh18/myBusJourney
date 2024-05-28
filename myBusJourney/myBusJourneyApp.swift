c821135f6167062babb0587c6b25c89bcd3d0771
Shireesh <shireesh18@gmail.com>
2024-05-28 13:21:31 +0100

    Initial Commit

---
+++ myBusJourney/myBusJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBusJourneyApp.swift
+//  myBusJourney
+//
+//  Created by Shireesh Marla on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct myBusJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
